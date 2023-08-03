35dcb4031a17eb76cdbc3ca964618c02a48b2b1d
赵雅国 <1248023357@qq.com>
2023-08-03 16:41:44 +0800

    ✨ (skills): Skills
    update skills

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -2,14 +2,26 @@
   <section>
     <h2>专业技能</h2>
     <ul>
-      <li>具备扎实的前端基本功 ES6 / HTML5 / CSS3，理解 web 标准和跨浏览器开发；</li>
+      <li> 熟练使用H5+C3快速构建页面，熟练使用less、sass等预处理器，有良好的编码习惯;</li>
+      <li> 熟练使用Vue2/3全家桶+JavaScript、TypeScript开发，并熟悉主流MVVM框架;</li>
+      <li> 熟练使用elementui、Naiveui、iView、Ant-Design等主流UI组件库;</li>
+      <li> 熟练使用Echarts、antv、hightcharts、D3等进行可视化数据展示，大屏开发;</li>
+      <li> 熟练使用Nuxt进行服务端渲染开发;   </li>
+      <li> 熟悉使用vuepress，vitepress进行文档搭建;</li>
+      <li> 熟悉使用Nest，Express，构建中间件服务和后端服务;</li>
+      <li> 熟悉Flutter、小程序、H5、uniapp开发;</li>
+      <li> 熟悉Vite2/3/4、Rollup、webpack，了解gulp、grunt; </li>
+      <li> 熟练使用Git进行代码分布式管理和团队协作;</li>
+      <li> 熟练进行项目排期规划、进度把控、分配任务、调研落地、制定规范、项目整体设计、组件设计等项目管理工作;</li>
+      <li> 富有激情，具有较强的学习能力，善于沟通、工作积极主动，责任心强，具有良好的团队协作和跨部门协调能力;</li>
+      <!-- <li>具备扎实的前端基本功 ES6 / HTML5 / CSS3，理解 web 标准和跨浏览器开发；</li>
       <li>熟悉掌握 DIV+CSS 布局，能够根据交互原型和UI设计图完整制作出网页；</li>
       <li>熟悉预处理 SCSS、Tailwind CSS 框架和 UI 框架 Element UI；</li>
       <li>熟悉使用前端主流框架Vue.js + Typescript 开发；</li>
       <li>熟悉移动端 web 页面的响应式布局、页面尺寸大小的自适应布局；</li>
       <li>熟悉 uniapp 小程序开发框架</li>
       <li>熟悉 webpack / vite 打包工具</li>
-      <li>熟悉 HTTP 的基础知识、Git 版本管理工具</li>
+      <li>熟悉 HTTP 的基础知识、Git 版本管理工具</li> -->
     </ul>
   </section>
 </template>
