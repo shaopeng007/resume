44ba44c43d9909d68a21fede7f4fb9cd4e3bda01
赵雅国 <1248023357@qq.com>
2023-08-03 16:40:38 +0800

    ✨ (UserInfo): Update UserInfo
    update userinfo

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -6,7 +6,7 @@ import avatar from '/avatar.png'
   <section>
     <div>
       <div>
-        <img class="float-none lg:float-right
+        <!-- <img class="float-none lg:float-right
         w-24 h-24 
         sm:w-28 sm:h-28
         md:w-32 md:h-32
@@ -15,13 +15,15 @@ import avatar from '/avatar.png'
         m-auto mb-10
         lg:m-6 lg:mb-0
 
-        rounded-full " :src="avatar" alt="avatar" />
+        rounded-full " :src="avatar" alt="avatar" /> -->
       </div>
-      <h3 class="text-center sm:text-left ">ooooshino</h3>
+      <h3 class="text-center sm:text-left ">GGupzHH</h3>
       <ul>
-        <li>1999 / 男 / 广东xx</li>
-        <li>本科 / 广东东软学院计算机系 / 23届应届生</li>
-        <li>技术博客：https://oshino.cc</li>
+        <li>1996 / 男 / 呼和浩特</li>
+        <li>本科 / 内蒙古科技大学 / 软件工程</li>
+        <li>技术博客：
+          <a href="https://ggupzhh.github.io/blog" target="_blank">https://ggupzhh.github.io/blog</a>
+        </li>
         <li>期望职位：web前端开发工程师</li>
         <li>手机：131-xxxx-xxxx（微信同号）</li>
         <li>邮箱：xxxx@gmail.com</li>
